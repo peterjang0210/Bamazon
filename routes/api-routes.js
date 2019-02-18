f90a211f62081061f0c485603cc942b3fec2faec
Peter Jang <peter.jang@outlook.com>
2019-02-18 18:56:44 -0500

    added get routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,5 +2,22 @@ const db = require(`../models`);
 
 module.exports = function (app) {
     app.get("/api/products", function(req, res){
-    })
+        db.Products.findAll({}).then(function(data){
+            res.json(data);
+        }).catch(function(error){
+            res.json({error: error});
+        });
+    });
+
+    app.get("/api/products/:productName", function(req, res) {
+        const selected = req.params.productName;
+        db.Products.findOne({where: {
+            name: selected
+        }}).then(function(data){
+            res.json(data);
+        }).catch(function(error){
+            res.json({error: error});
+        });
+    });
+
 }
