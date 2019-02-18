47ac2a509f28b67be56e47668dfc04135ed47c70
Peter Jang <peter.jang@outlook.com>
2019-02-18 18:51:33 -0500

    added html-route and loaded packages

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/api-routes.js')(app);
-require('./routes/html-routes.js')(app);
+app.use(express.static(path.join(__dirname, "public")));
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-      console.log('App listening on PORT ' + PORT);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log('App listening on PORT ' + PORT);
     });
-  });
-  
+});
