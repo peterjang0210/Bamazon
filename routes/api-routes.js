aebd73d916972d70ac11360ac46a9a9d6045d1de
Peter Jang <peter.jang@outlook.com>
2019-02-19 18:53:22 -0500

    made basic api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,23 +1,59 @@
-const db = require(`../models`);
+const db = require("../models");
 
 module.exports = function (app) {
-    app.get("/api/products", function(req, res){
-        db.Products.findAll({}).then(function(data){
+    app.get("/api/products", function (req, res) {
+        db.Product.findAll({}).then(function (data) {
             res.json(data);
-        }).catch(function(error){
-            res.json({error: error});
+        }).catch(function (error) {
+            res.json({ error: error });
         });
     });
 
-    app.get("/api/products/:productName", function(req, res) {
-        const selected = req.params.productName;
-        db.Products.findOne({where: {
-            name: selected
-        }}).then(function(data){
+    app.get("/api/products/:id", function (req, res) {
+        db.Product.findOne({
+            where: {
+                product_name: req.params.productName
+            }
+        }).then(function (data) {
             res.json(data);
-        }).catch(function(error){
-            res.json({error: error});
+        }).catch(function (error) {
+            res.json({ error: error });
         });
     });
 
+    app.post("/api/products", function (req, res) {
+        db.Product.create(req.body)
+            .then(function (data) {
+                res.json("success");
+            }).catch(function (error) {
+                res.json({ error: error });
+            });
+    });
+
+    app.put("/api/products/:id", function (req, res) {
+        db.Product.update(
+            req.body,
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        ).then(function (data) {
+            res.json("success");
+        }).catch(function (error) {
+            res.json({ error: error });
+        });
+    });
+
+    app.delete("/api/products/:id", function (req, res) {
+        db.Product.destroy(
+            {
+                where:
+                    { id: req.params.id }
+            }).then(function (data) {
+                res.json("success");
+            }).catch(function(error){
+                res.json({error: error});
+            });
+    });
 }
